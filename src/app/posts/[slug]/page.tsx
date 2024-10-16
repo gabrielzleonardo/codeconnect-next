22fbb3897ab5ece175bab75eb2aa30827314810f
Gabriel Leonardo <gleonardoalano@gmail.com>
2024-10-16 14:49:40 -0300

    style: removed unecessary style

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -9,7 +9,7 @@ const getPost = async (slug: string) => {
   try {
     const post = await db.post.findUnique({
       where: { slug },
-      include: { author: true },
+      include: { author: true, comments: true },
     });
 
     if (!post) throw new Error(`Post with slug ${slug} not found`);
@@ -36,7 +36,7 @@ const PostPage = async ({ params: { slug } }: { params: { slug: string } }) => {
     <div className="space-y-6">
       <PostCard post={post} highlight />
       <div className="space-y-2">
-        <h2 className="paragraph-lg font-semibold text-gray-200">Código:</h2>
+        <h2 className="paragraph-lg font-semibold">Código:</h2>
         <div className="bg-gray-400 p-4 text-gray-100 font-mono shadow-[0px_8px_24px_0px_rgba(0,0,0,0.24)] rounded-lg">
           <div
             className="lg:max-h-[220px] md:max-h-[260px] overflow-y-auto text-[15px] custom-scrollbar"
